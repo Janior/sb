d80ba1e8c6814fdd43b858ce2421365abdfea5d2
Janior <janior_orlando@hotmail.com>
2018-10-25 11:55:03 -0700

    Agrega boton publicar post en la lista de post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,17 @@ class PostsController < ApplicationController
 		@posts = Post.new
 	end		
 
+	def create
+		@posts = Post.new(post_params)
+		if @posts.save
+			redirect_to posts_path, notice: "El post fue publicado con EXITO"
+		else
+			render :new
+		end
+	end				
+
+	private 
+		def post_params
+			params.require(:post).permit(:titulo, :cuerpo)
+		end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/posts', to: 'posts#index'
-  get '/posts/new', to: 'posts#new'
-  
+  get '/posts/new', to: 'posts#new', as: 'new_post'
+  post '/posts', to: 'posts#create'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
