154bad0b14853c8185a46db674d9380a521f5b3d
Janior <janior_orlando@hotmail.com>
2018-10-25 16:33:46 -0700

    Crea ruta /patch/edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -24,6 +24,15 @@ class PostsController < ApplicationController
 		@posts = Post.find(params[:id])
 	end
 
+	def update
+		@posts = Post.find(params[:id])
+		if @posts.update(post_params)
+			redirect_to posts_path, notice: "El post fue modificadoo con EXITO"
+		else
+			render :edit
+		end
+	end
+
 	private 
 		def post_params
 			params.require(:post).permit(:titulo, :cuerpo)
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   post '/posts', to: 'posts#create'
   get '/posts/:id', to: 'posts#show', as: 'post'
   get '/posts/:id/edit', to: 'posts#edit'
+  patch '/posts/:id', to: 'posts#update'
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
